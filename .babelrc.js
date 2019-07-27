1614ae341673cfee220a2d9680c1500323c87500
sibiraj-s <dev.sibiraj@outlook.com>
2019-07-27 11:02:59 +0530

    chore: update comments in babelrc

--- .babelrc.js
+++ .babelrc.js
@@ -14,8 +14,7 @@ const babelRc = (api) => {
         {
           // Allow importing core-js in entrypoint and use browserlist to select polyfills
           useBuiltIns: 'entry',
-          // Set the corejs version we are using to avoid warnings in console
-          // This will need to change once we upgrade to corejs@3
+          // Use corejs@3
           corejs: 3,
           // Do not transform modules to CJS
           modules: false,
