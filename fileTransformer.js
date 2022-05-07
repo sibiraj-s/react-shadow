de81dc79bb78ceee5b43d039928b9a024dcab70c
Sibiraj <dev.sibiraj@outlook.com>
2022-05-07 23:59:41 +0530

    build(deps): bump jest to v28

--- fileTransformer.js
+++
@@ -1,7 +0,0 @@
-import path from 'node:path';
-
-export default {
-  process(_, filename) {
-    return `export default ${JSON.stringify(path.basename(filename))};`;
-  },
-};
--- jest.config.js
+++ jest.config.js
@@ -175,7 +175,6 @@ export default {
   // A map from regular expressions to paths to transformers
   transform: {
     '^.+\\.js(x)?$': 'babel-jest',
-    '\\.svg$': '<rootDir>/fileTransformer.js',
   },
 
   // An array of regexp pattern strings that are matched against all source file paths,
